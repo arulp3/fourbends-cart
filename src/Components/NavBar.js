5211cb82498485d5653cfd77c0c2ad80245b46e3
Arul Prasad P <66033128+arulp3@users.noreply.github.com>
2022-05-14 13:41:02 +0530

    Final Commit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../Components/NavBar.css'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {Nav} from 'react-bootstrap'
 import { useSelector } from 'react-redux'
+
 
 const NavBar = () => {
 
   const  cartValue = useSelector((state) => state.cart.value)
   const  loggedinName = useSelector((state) => state.cart.name)
   const  logginStatus = useSelector((state) => state.cart.logginStatus)
+
+  const navigate = useNavigate()
   
   return (
     <div className='navbar'>
@@ -21,7 +24,7 @@ const NavBar = () => {
     <Nav.Link><Link to="/cart">Cart-{cartValue.length}</Link></Nav.Link>
   </Nav.Item>
   <Nav.Item>
-    {logginStatus === false ? <Nav.Link><Link to="/login">Login</Link></Nav.Link> : <Nav.Link><li>logoff</li></Nav.Link>}
+    {logginStatus === false ? <Nav.Link><Link to="/login">Login</Link></Nav.Link> : <Nav.Link><li style={{listStyle : "none"}} onClick={()=>{navigate("/login")}}>logoff</li></Nav.Link>}
   </Nav.Item>
   <Nav.Item>
     
