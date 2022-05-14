157a83181e553066516d0881602b829609b2771c
Arul Prasad P <66033128+arulp3@users.noreply.github.com>
2022-05-14 13:36:55 +0530

    Update Cart.jsx

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -11,17 +11,6 @@ function Cart() {
   const price = cartCount.map((item)=>item.product_price)
   const totalPrice = price.reduce((a, b) => a + b, 0);
 
-  // const counts = {}
-  // cartCount.forEach((x) => {
-  //   counts[x] = (counts[x] || 0) + 1
-  // })
-  // const keys = Object.keys(counts)
-  // const vals = Object.values(counts)
-  React.useEffect(() => {
-    // console.log(keys)
-
-    console.log(totalPrice)
-  }, [])
 
   return (
     <div className="cartItems">
